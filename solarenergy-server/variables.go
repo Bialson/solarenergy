1cf43c1ce4bb9d2dc917801dd682b218438d453b
Bialson <bialson95@gmail.com>
2023-02-08 21:43:12 +0100

    New variables added

--- solarenergy-server/variables.go
+++ solarenergy-server/variables.go
@@ -3,7 +3,8 @@ package main
 // Variables for API DBW request
 
 const (
-	DATA_CAT    = 1002
+	DATA_CAT_1  = 1002
+	DATA_CAT_2  = 232
 	PERIOD      = 282
 	SECTION_1   = 156
 	SECTION_2   = 865
@@ -23,10 +24,14 @@ var Variables = map[int]string{
 }
 
 var Units = map[int]string{
+	158: "[TJ]",
 	186: "[MWh]",
 	187: "[kWh]",
 	188: "[kWh] - na 1 mieszkańca",
 	189: "[kWh] - na 1 odbiorcę",
+	240: "[ktoe]",
+	241: "[Mtoe]",
+	242: "[GWh]",
 }
 
 var Regions = map[int]string{
@@ -54,10 +59,25 @@ var Regions = map[int]string{
 
 var Types = map[int]string{
 	1273014: "Energia słoneczna",
-	1273015: "Energia wiatrowa",
-	7065509: "Energia geotermalna",
-	7065599: "Energia wodna",
+	1273015: "Pompa ciepła",
+	7065497: "Energia odnawialna i biopaliwa",
+	7065503: "Energia wodna",
+	7065509: "Energia wiatrowa",
 	7065591: "Energia z ogniw fotowoltaicznych",
+	7065599: "Energia geotermalna",
+	7065605: "Pierwotne biopaliwa stałe",
+	7065612: "Węgiel drzewny",
+	7065618: "Biogaz",
+	7065626: "Odnawialne odpady komunalne",
+	7072409: "Biopaliwa ciekłe - bioetanol",
+	7072413: "Domieszka bioetanolu w benzynie",
+	7072600: "Biopaliwa ciekłe - biodiesel",
+	7072606: "Domieszka biodieselu w oleju napędowym",
+	7072617: "Bio nafta do silników odrzutowych",
+	7072623: "Domieszka biopaliw w nafcie lotniczej",
+	7072632: "Biopłyny",
+	7072658: "Bioenergia",
+	7072668: "Energia z kolektorów słonecznych",
 }
 
 // var Tags = map[int]string{}
